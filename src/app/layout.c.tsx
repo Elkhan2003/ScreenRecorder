20c917f1bf4e9fadec5a10539bfdb4315c2758ba
Elcho911 <boss.armsport@gmail.com>
2024-12-20 01:01:59 +0600

    fix bug

--- src/app/layout.c.tsx
+++ src/app/layout.c.tsx
@@ -1,6 +1,12 @@
 "use client";
 import { FC, ReactNode } from "react";
-import ScreenRecordingProvider from "@/providers/ScreenRecordingProvider";
+import dynamic from "next/dynamic";
+const ScreenRecordingProvider = dynamic(
+	() => import("@/providers/ScreenRecordingProvider"),
+	{
+		ssr: false, // Ensures it only runs client-side
+	}
+);
 
 interface ILayoutClientProps {
 	children: ReactNode;
